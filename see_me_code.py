621199bc057164d07c01618e4d148a3225ec3187
Francisco José Marques Vieira <francisco.vieira@co.sapo.pt>
2014-05-18 23:18:38 +0100

    Allow disabling

--- see_me_code.py
+++ see_me_code.py
@@ -13,24 +13,33 @@ def get_buffer_contents(view):
 
 
 class SeeMeCode(sublime_plugin.EventListener):
+    def __init__(self):
+        self.settings = sublime.load_settings('SeeMeCode.sublime-settings')
+        self.settings.add_on_change('enabled', self.update_enabled)
+        self.settings.add_on_change('server', self.reconnect)
+        self.settings.add_on_change('port', self.reconnect)
+        self.update_enabled()
+
+    def update_enabled(self):
+        self.enabled = self.settings.get('enabled')
+
     def ensure_started(self):
         if not hasattr(self, 'io'):
-            print('SeeMeCode: Connecting to server')
-            self.settings = sublime.load_settings('SeeMeCode.sublime-settings')
-            self.settings.add_on_change('server', self.reconnect)
-            self.settings.add_on_change('port', self.reconnect)
             self.reconnect()
 
     def reconnect(self):
-        self.io = socketIO_client.SocketIO(self.settings.get('server'), self.settings.get('port'))
+        if self.enabled:
+            print('SeeMeCode: Connecting to server')
+            self.io = socketIO_client.SocketIO(self.settings.get('server'), self.settings.get('port'))
 
     def send_contents(self, view):
-        self.ensure_started()
-        buffer_contents = get_buffer_contents(view)
-        try:
-            self.io.emit('write', {'content': buffer_contents})
-        except socketIO_client.exceptions.ConnectionError:
-            self.reconnect()
+        if self.enabled:
+            self.ensure_started()
+            buffer_contents = get_buffer_contents(view)
+            try:
+                self.io.emit('write', {'content': buffer_contents})
+            except socketIO_client.exceptions.ConnectionError:
+                self.reconnect()
 
     def on_modified(self, view):
         self.send_contents(view)
